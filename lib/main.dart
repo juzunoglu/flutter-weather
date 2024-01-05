2d361f9e6059e6b3d665e872b74f54725b1e7184
Alihan Uzunoglu <uzunoglualihan@gmail.com>
2024-01-05 18:41:12 +0300

    Create the Home Widget to Enable Hot Reload!

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-  runApp(MaterialApp(
-    home: Scaffold(
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('My First App'),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent[800],
+        backgroundColor: Colors.blueAccent,
       ),
       body: const Center(
         child: Text(
           'Hello!',
           style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.amber
-          ),
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.blueAccent),
         ),
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.blueAccent,
-        child: Text('Click'),
-      ),
-    ),
-  ));
+      // floatingActionButton: const FloatingActionButton(
+      //   onPressed: () {},
+      //   backgroundColor: Colors.blueAccent,
+      //   child: Text('Click'),
+      // ),
+    );
+  }
+}
